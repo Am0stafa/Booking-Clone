80c9abb5e51fefd92d28f9000391b540e09c4ff8
Abdelrahman mostafa <a.abdo.mae@gmail.com>
2022-07-31 03:54:41 +0200

    done all the crud operations for the user , hotel , and room

---
+++ admin/src/hooks/useFeatch.js
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+
+const useFetch = (url,dataError=false) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const res = await axios.get(url);
+        if (dataError)
+          setData(res.data.data);
+        else
+          setData(res.data);
+      } catch (err) {
+        setError(err);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, [url]);
+
+  const reFetch = async () => {
+    setLoading(true);
+    try {
+      const res = await axios.get(url);
+      if (dataError)
+          setData(res.data.data);
+        else
+          setData(res.data);
+    } catch (err) {
+      setError(err);
+    }
+    setLoading(false);
+  };
+
+  return { data, loading, error, reFetch };
+};
+
+export default useFetch;
--- api/model/userModel.js
+++ api/model/userModel.js
@@ -1,40 +1,41 @@
 const mongoose = require('mongoose');
-const UserSchema = new mongoose.Schema(
-    {
-      username: {
-        type: String,
-        required: true,
-        unique: true,
-      },
-      email: {
-        type: String,
-        required: true,
-        unique: true,
-      },
-      country: {
-        type: String,
-        required: true,
-      },
-      img: {
-        type: String,
-      },
-      city: {
-        type: String,
-        required: true,
-      },
-      phone: {
-        type: String,
-        required: true,
-      },
-      password: {
-        type: String,
-        required: true,
-      },
-      isAdmin: {
-        type: Boolean,
-        default: false,
-      },
+const UserSchema = new mongoose.Schema(  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
     },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+    img: {
+      type: String,
+      default:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true },
     { timestamps: true }//! to give created at and deleted at time
   );
   const User = mongoose.model('User', UserSchema);
